c25fdbc99de1ed078b4d711ef1fd4379ed1bd298
IGTES <itias89@gmail.com>
2022-05-17 18:55:48 +0300

    initial

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-(function () {
-    const burger = document.querySelector('.burger');
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('burger_active');
-    });
-}());
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu');
+burgerMenu.addEventListener('click',function(){
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
